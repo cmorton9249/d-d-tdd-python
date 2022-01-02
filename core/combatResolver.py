933e54b09d94d6f4d0796907ed5a9effb8b6efdd
Chris Morton <christopher_morton@hotmail.com>
2022-01-02 17:00:05 -0500

    starting Feature: Character can be damaged

--- core/combatResolver.py
+++ core/combatResolver.py
@@ -1,3 +1,9 @@
+from core.character import Character
+
+
 class CombatResolver:
     def __init__(self) -> None:
         pass
+
+    def resolveCombat(self, attacker: Character, defender: Character) -> None:
+        characterAttackRoll = attacker.attack()
--- core_tests/combatResolver_test.py
+++ core_tests/combatResolver_test.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
 from core.character import Character
 from core.combatResolver import CombatResolver
+from unittest.mock import Mock
 
 
 class CombatResolverTests(TestCase):
     def setUp(self) -> None:
-        self._attacker = Character()
-        self._defender = Character()
+        self._attacker = Mock(wraps=Character())
+        self._defender = Mock(wraps=Character())
         self._systemUnderTest = CombatResolver()
         return super().setUp()
 
-    def combatResolver_manages_attacks_between_two_parties():
-        assert True == True
+    def combatResolver_manages_attacks_between_two_parties(self):
+        self._systemUnderTest.ResolveCombat(self._attacker, self._defender)
+
+        self._attacker.roll.assert_called_once()
